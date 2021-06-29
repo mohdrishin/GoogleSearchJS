d044c25fea4b23ba8b5859cdee3e6f898bac9148
rishin <mrishinv@gmail.com>
2021-06-29 20:44:17 +0530

    dark mode added

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 var googleIcon = document.getElementById("googleIcon");
 var searchBox = document.getElementById("search-box");
+var darkModeOn = document.body;
 
+//searchbox function
 googleIcon.onclick = function() {
     searchBox.classList.toggle("active");
 }
+
+
+
+//darkmode function - myself tried - working
+var inbuilt = document.getElementById("darkmode");
+
+darkmode.onclick = function() {
+    darkModeOn.style.backgroundColor = "#000"; 
+    document.getElementById("darkmode").innerText = "Dark Mode OFF";
+}
+
+//darkmode second method
+
